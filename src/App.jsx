6d3e69dcfd54f0de0ab1ce0eeb62ddb5daf70cd3
NSRoberts <nakkia.roberts@outlook.com>
2021-10-13 00:20:36 -0700

    Working on landing page with styling.

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState }from 'react';
+import './App.css';
+
+function App() {
+
+const bridalprofile = []
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <center><h1>The Social Bride</h1></center>
+                <center><h3>Social Interaction and Bridal Planning</h3></center>
+                <div className="layout">
+                <div>Bridal Profile</div>
+                <div>Bridal Plan</div>
+                <div>Social</div>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default App;
